7078ac5d7488122421868edb7b66d4f1b3786ae4
Ben Whittington <benwhittington@pop-os.localdomain>
2020-06-04 10:32:54 +1200

    add data

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-
+import numpy as np
 
 def dp():
     no_weeks = 13
     inventory_space = 10  # a
     max_sales = 5  # u_n
+    prices = [1, 2, 3, 1, 2, 2, 5, 4, 1, 1, 4, 4, 2]
+    production = [2, 1, 2, 1, 2, 2, 2, 1, 2, 3, 2, 1, 0]
+
+    cost = np.zeros((max_sales, no_weeks))
+
+    for n in range(no_weeks):
 
 
 
 def main():
-    pass
+    dp()
 
 
 if __name__ == "__main__":
